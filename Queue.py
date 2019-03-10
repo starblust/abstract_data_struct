8f829c0f4fdf363c6d60d260fdd5a55b0dc1ddb7
Artemij <rtmjrtmv@gmail.com>
2019-03-10 21:27:25 +0300

    add Stack and Queue

---
+++ Queue.py
@@ -0,0 +1,17 @@
+class Queue:
+    def __init__(self):
+        self.q = []
+
+    def isEmpty(self):
+        return self.q == []
+
+    def enqueue(self, item):
+        self.q.insert(0, item)
+
+    def dequeue(self):
+        if (self.isEmpty()):
+            return None
+        return self.q.pop()
+
+    def size(self):
+        return len(self.q)
---
+++ Stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if (self.isEmpty()):
+            return None
+        return self.items.pop()
+
+    def peek(self):
+        if (self.isEmpty()):
+            return None
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+stack = Stack()
+stack.push('first item')
+print(stack.pop())
+print(stack.pop())
