70d4715df5b60cfb434f08edcc43418e269d1e3d
Artemij <rtmjrtmv@gmail.com>
2019-03-12 22:38:33 +0300

    unordered list

---
+++ UnorderedList.py
@@ -0,0 +1,47 @@
+class UnorderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head is None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.setNext(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            count = count + 1
+            current = current.getNext()
+        
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current is not None:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        found = False
+        previous = None
+        while not found:
+            if (current.getData() == item):
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous is None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
